18d7d51dd22ab571168468b9e92d4be4b661e38f
Anay1440 <anay.patil@gmail.com>
2022-10-17 23:59:34 +0530

    OST Lab 7 Complete

--- ostLab/Lab7-CSSAndJS/scripts/bouncingBall.js
+++ ostLab/Lab7-CSSAndJS/scripts/bouncingBall.js
@@ -3,12 +3,41 @@ const context = canvas.getContext('2d');
 const canvasHeight = canvas.height;
 const canvasWidth = canvas.width;
 
-var x = 20;
-var y = 20;
+var x = 25;
+var y = 30;
 var vx = 1;
 var vy = 2;
 var radius = 20;
 
 function render() {
-
+    requestAnimationFrame(render);
+ 
+        // It clears the specified pixels within
+        // the given rectangle
+        context.clearRect(0, 0, canvasWidth, canvasHeight);
+ 
+        // Creating a circle
+        context.beginPath();
+        context.strokeStyle = "black";
+        context.arc(x, y, radius, 0, Math.PI * 2, false);
+        context.stroke();
+ 
+        // Conditions so that the ball bounces
+        // from the edges
+        if (radius + x > canvasWidth)
+            vx = 0 - vx;
+ 
+        if (x - radius <= 0)
+            vx = 0 - vx;
+ 
+        if (y + radius > canvasHeight)
+            vy = 0 - vy;
+ 
+        if (y - radius <= 0)
+            vy = 0 - vy;
+ 
+        x = x + vx;
+        y = y + vy;
 }
+
+render();
